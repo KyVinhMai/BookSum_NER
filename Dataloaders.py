a085520385490d8373a01e006f652ad316a99f80
Arseny <sensailing@gmail.com>
2023-04-05 17:20:36 -0600

    More gpt api processing machinery

--- Dataloaders.py
+++ Dataloaders.py
@@ -1,6 +1,6 @@
 #import entity_replacement
 from utils.BookProcessing import chunk_book, rechunk_book
-from gpt_api_processing import SummarizeChunk
+from gpt_api_processing import SummarizeChunk, SummarizeChunkRetry
 import json
 
 
@@ -22,6 +22,10 @@ class BookProcessor():
 
         self.book_summary_so_far = None
 
+        self.failed_summary_inds = list()
+        self.failed_summary_comments = list()
+
+
         self.data_list = []
 
         self.live_mode = live_mode
@@ -40,6 +44,9 @@ class BookProcessor():
 
         for current_chunk_index in range(num_chunks_to_process):
 
+            if current_chunk_index % 10 == 0:
+                print("Current index: {} out of {}".format(current_chunk_index + 1, num_chunks_to_process))
+
             current_chunk = self.book_chunks[current_chunk_index]
 
             if current_chunk_index > 0:
@@ -48,8 +55,38 @@ class BookProcessor():
             if current_chunk_index < len(self.book_chunks) - 1:
                 current_chunk = current_chunk + self.book_chunks[current_chunk_index + 1][:300]
 
-            self.book_chunk_summaries.append(SummarizeChunk(current_chunk))
-            self.current_chunk_index += 1
+            try:
+                self.book_chunk_summaries.append(SummarizeChunk(current_chunk))
+
+            except ValueError as first_error:
+
+                print("Retrying to force re-summarize chunk")
+
+                failures = 0
+                while failures < 3:
+
+                    try:
+                        summary, warning = SummarizeChunkRetry(current_chunk)
+
+                        self.book_chunk_summaries.append(summary)
+
+                        if warning is not None:
+                            self.failed_summary_inds.append(len(self.book_chunk_summaries) -1)
+                            self.failed_summary_comments.append(warning)
+                        else:
+                            print("Successfully re-summarized the chunk using a more forceful request.")
+                        break
+
+                    except ValueError as e:
+                        print(e)
+                        print("Retrying to summarize chunk")
+                        failures += 1
+
+                if failures == 3:
+                    self.book_chunk_summaries.append("This book chunk can not be summarized.")
+                    self.failed_summary_inds.append(len(self.book_chunk_summaries) - 1)
+                    self.failed_summary_comments.append("Complete failure.")
+                    print("Failed to summarize chunk")
 
     def create_recognition_questions(self):
         ''' Creates recognition questions:
@@ -60,14 +97,19 @@ class BookProcessor():
          the event that is being tested was introduced in the book.'''
 
         num_chunks_to_process = len(self.book_chunks) if self.live_mode else 10
-
+        raise NotImplementedError() # Finish
 
 if __name__ == "__main__":
     with open("Data/RawBooks/Example.txt", "r") as f:
         b = f.read()
 
-    book_processor = BookProcessor(b, live_mode=False)
-    book_processor.create_chunk_summaries()
+
+    if False:
+        book_processor = BookProcessor(b, live_mode=True)
+        book_processor.create_chunk_summaries()
+
+       # with open("Data/AnalysisExamples/PrideAndPrejudiceSummaries.txt", "w") as f:
+       #     f.writelines(book_processor.book_chunk_summaries)
 
 
 
--- gpt_api_processing.py
+++ gpt_api_processing.py
@@ -1,6 +1,8 @@
 import os
 import openai
 
+from utils.BookProcessing import rechunk_book
+
 BEGIN_ANSWER_TAG = "### BEGIN ANSWER ###"
 
 #openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -50,7 +52,8 @@ def SummarizeChunk(chunk):
   response_content = response["choices"][0]["message"]["content"]
 
   if BEGIN_ANSWER_TAG not in response_content:
-    raise ValueError("GPT response does not have a {} tag".format(BEGIN_ANSWER_TAG))
+
+    raise ValueError("GPT response does not have a {} tag. Response: {}".format(BEGIN_ANSWER_TAG, response_content))
 
   resp_parts = response_content.split(BEGIN_ANSWER_TAG)
 
@@ -63,6 +66,56 @@ def SummarizeChunk(chunk):
     raise ValueError("GPT gave an empty response")
 
   return answer
+
+def SummarizeChunkRetry(chunk):
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "You are a helpful assistant that summarizes book snippets. Begin your answer with a {} tag.".format(BEGIN_ANSWER_TAG)},
+      {"role": "user", "content": 'Summarize the following book excerpt: "{}". Make sure to begin your answer with a {} tag!'.format(chunk, BEGIN_ANSWER_TAG)} # Add "in under 500 words?
+    ]
+  )
+
+  response_content = response["choices"][0]["message"]["content"]
+
+  if BEGIN_ANSWER_TAG not in response_content:
+
+    return response_content, "GPT response does not have a {} tag. Response: {}".format(BEGIN_ANSWER_TAG, response_content)
+
+  resp_parts = response_content.split(BEGIN_ANSWER_TAG)
+
+  if len(resp_parts) != 2:
+    raise ValueError("GPT gave multiple {} tags".format(BEGIN_ANSWER_TAG))
+
+  answer = resp_parts[-1]
+
+  if not answer.strip():
+    raise ValueError("GPT gave an empty response")
+
+  return answer, None
+
+def SummarizeSummaries(chunk_summaries, max_length=10000):
+
+  meta_chunks = rechunk_book(["Summary {}: {}\n".format(ind, summary) for ind, summary in enumerate(chunk_summaries)], max_length)
+  result = []
+
+  for ind, m in enumerate(meta_chunks):
+    if ind % 10 == 0:
+      print("Processed {} meta chunks".format(ind + 1))
+
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+        {"role": "system",
+         "content": "You are a helpful assistant that summarizes book scene summaries. Begin your answer with a {} tag.".format(
+           BEGIN_ANSWER_TAG)},
+        {"role": "user",
+         "content": 'Summarize the following book excerpt summaries into one summary: "{}". Make sure to begin your answer with a {} tag!'.format(m, BEGIN_ANSWER_TAG)}  # Add "in under 500 words?
+      ]
+    )
+    result.append(response["choices"][0]["message"]["content"])
+
+  return result
 
 def CreateFalseSummary(chunk):
   response = openai.ChatCompletion.create(
