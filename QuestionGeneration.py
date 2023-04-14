4b4fe9b937d791b48fd43ca4c0e567ecd01af579
Arseny <sensailing@gmail.com>
2023-04-13 19:08:55 -0600

    Change and lookahead decoy question generation done!

--- QuestionGeneration.py
+++ QuestionGeneration.py
@@ -7,6 +7,8 @@ import numpy as np
 random.seed(42)
 np.random.seed(42)
 
+from collections import Counter
+
 class RecognitionQuestion:
 
     def __init__(self, correct_answer, decoys, decoy_types, when_asked, retention_delay):
@@ -15,9 +17,11 @@ class RecognitionQuestion:
         self.decoys = decoys
         self.decoy_types = decoy_types
         self.when_asked = when_asked
-        self.retention_delay = when_asked
+        self.retention_delay = retention_delay
 
-        self.is_mixed = True if len(set(decoy_types)) == 1 else False
+        self.is_mixed = True if len(set(decoy_types)) > 1 else False
+
+        self.question_type = "mixed" if self.is_mixed else self.decoy_types[0]
 
         self.n_options = len(self.decoys) + 1
         self.true_ans_position = np.random.randint(self.n_options)
@@ -25,9 +29,30 @@ class RecognitionQuestion:
         self.all_options = decoys.copy()
         self.all_options.insert(self.true_ans_position, correct_answer)
 
-
     def question_string(self):
-        return "".join(["Which of the following scenes were in the book?\n"] + [str(i + 1) + ") " + self.all_options[i] + "\n" for i in range(self.n_options)])
+        return "".join(["Which of the following scenes was in the book?\n"] + [str(i + 1) + ") " + self.all_options[i] + "\n" for i in range(self.n_options)])
+
+    def detailed_question_string(self):
+
+        description = "A {} decoy type recognition question:\n".format(self.question_type)
+
+
+        answer_options = []
+        i = 0
+        dec_i = 0
+        while i < self.n_options:
+            if i != self.true_ans_position:
+                answer_options.append("({} decoy) ".format(self.decoy_types[dec_i]) + str(i + 1) + ") " + self.all_options[i] + "\n")
+                dec_i += 1
+            else:
+                answer_options.append("(True answer) " + str(i + 1) + ") " + self.all_options[i] + "\n")
+
+            i += 1
+
+
+        question = "".join(["Which of the following scenes was in the book?\n"] + answer_options)
+
+        return description + question
 
     def __str__(self):
 
@@ -35,11 +60,119 @@ class RecognitionQuestion:
 
         return self.question_string() + answer
 
+    def __repr__(self):
 
+        return self.detailed_question_string()
+
+    def IsComplete(self):
+
+        if self.correct_answer is None or any([el is None for el in self.decoys]):
+            return False
+        else:
+            return True
 
 class RecognitionQuestionGenerator:
 
-    def __init__(self, book_processors):
-        '''Takes a list of book processors (already with true and fake summaries generated. Generates questions based on that.'''
-        self.book_processors
+    def __init__(self, book_processor, parallel_books=None):
+        '''Takes a book processor (already with true and fake summaries generated. Generates questions based on that.'''
+        self.book_processor = book_processor
+        self.parallel_books = parallel_books ### A list of book processor objects, to draw decoy answers from. Only relevant for the "other book" type of recognition decoys.
 
+        self.questions = [] # A list that stores a list of RecognitionQuestion objects as its elements.
+        self.read_progress = 0 # Index of the last read chunk.
+
+        self.book_length = len(self.book_processor.book_chunk_summaries)
+
+        # By default, false summaries may have None values where false summary generation failed
+        self.false_summaries_filtered = [[s for s in fs if s] for fs in self.book_processor.false_book_chunk_summaries]
+        self.false_summary_chunk_weights = np.array([len(el) for el in self.false_summaries_filtered])
+
+
+
+    def advance(self):
+
+        current_questions = []
+        for _ in range(settings.lookahead_questions_per_step):
+            q = self.generate_lookahead_question()
+            if q and RecognitionQuestion.IsComplete(q):
+                current_questions.append(q)
+
+        for _ in range(settings.scene_negation_questions_per_step):
+            q = self.generate_scene_negation_question()
+
+            if q and RecognitionQuestion.IsComplete(q):
+                current_questions.append(q)
+
+        for _ in range(settings.other_book_questions_per_step):
+            q = self.generate_other_book_question()
+
+            if q and RecognitionQuestion.IsComplete(q):
+                current_questions.append(q)
+
+        self.read_progress += 1
+        self.questions.append(current_questions)
+
+    def generate_lookahead_question(self):
+        if self.read_progress > self.book_length - (settings.number_of_decoy_options + 1):
+            return None # Can not generate lookahead questions too close to the end of the book
+        else:
+
+            decoy_ids = np.random.choice(np.arange(self.read_progress + 1, self.book_length), settings.number_of_decoy_options, replace=False)
+            true_idx = np.random.randint(self.read_progress + 1)
+
+            decoys = [self.book_processor.book_chunk_summaries[id] for id in decoy_ids]
+            decoy_types = ["Lookahead" for _ in decoy_ids]
+
+            true_ans = self.book_processor.book_chunk_summaries[true_idx]
+
+            return RecognitionQuestion(correct_answer=true_ans, decoys=decoys, decoy_types=decoy_types,
+                                       when_asked=self.read_progress, retention_delay=self.read_progress-true_idx)
+
+    def generate_scene_negation_question(self):
+
+        available_fake_weights = self.false_summary_chunk_weights[:self.read_progress + 1].copy()
+
+        if np.sum(available_fake_weights) < settings.number_of_decoy_options:
+            return None
+        else:
+
+            decoys = []
+
+
+            idx_to_numpicks = Counter()
+            for _ in range(settings.number_of_decoy_options):
+
+                fake_weights_normalized = available_fake_weights / np.sum(available_fake_weights)
+
+                chunk_choice = np.random.choice(np.arange(self.read_progress + 1), p=fake_weights_normalized)
+                available_fake_weights[chunk_choice] -= 1
+
+                idx_to_numpicks[chunk_choice] += 1
+
+            for chunk, numpicks in idx_to_numpicks.items():
+                decoys_from_chunk = np.random.choice(self.false_summaries_filtered[chunk], numpicks, replace=False)
+                decoys.extend(decoys_from_chunk)
+
+            np.random.shuffle(decoys) # Otherwise decoys from the same chunk would be close
+            decoy_types = ["Change" for _ in range(settings.number_of_decoy_options)]
+
+            true_idx = np.random.randint(self.read_progress + 1)
+            true_ans = self.book_processor.book_chunk_summaries[true_idx]
+
+        return RecognitionQuestion(true_ans, decoys, decoy_types, when_asked=self.read_progress, retention_delay=self.read_progress-true_idx)
+
+    def generate_other_book_question(self):
+        raise NotImplementedError
+
+    def generate_questions(self):
+
+        while self.read_progress < self.book_length:
+            self.advance()
+
+
+if __name__ == "__main__":
+    b = dl.BookProcessor.init_from_summaries("./Data/TrueAndFalseSummaryData/ScifiExample25chunks.tagseparated")
+    question_generator = RecognitionQuestionGenerator(b)
+    question_generator.generate_questions()
+
+    print(question_generator.questions[0])
--- settings.py
+++ settings.py
@@ -3,3 +3,12 @@ fake_summary_separator = "###THISISOURUNIQUESUMMARYSEPARATORUNLIKELYTOBEENCOUNTE
 line_separator = "###THISISOURUNIQUELINESEPARATORUNLIKELYTOBEENCOUNTEREDINANYBOOK###"
 
 debug_num_chunks = 25
+
+
+
+### Different Recognition Questions per step
+lookahead_questions_per_step = 1
+scene_negation_questions_per_step = 1
+other_book_questions_per_step = 0
+
+number_of_decoy_options = 4
